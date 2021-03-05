296d692a4bb8586ac2abd42d548b80173f563834
Andrea Lamego <andrea.lamego.soriano@outlook.com>
2021-03-04 20:38:16 -0600

    BuildingAPI

--- book-api.js
+++ book-api.js
@@ -15,7 +15,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post('/book', (req, res) => {
-    // We will be coding here
+    const book = req.body;
+
+    // Output the book to the console for debugging
+    console.log(book);
+    books.push(book);
+
+    res.send('Book is added to the database');
 });
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`)); 
